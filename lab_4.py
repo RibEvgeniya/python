b6cce6c99b4412c8f448ec5f871e6f04c83163a1
Evgeniya Rib <--->
2024-02-17 21:26:01 +0300

    lab_4 task 2

--- lab_4.py
+++ lab_4.py
@@ -26,3 +26,29 @@ f.close()
 g.close()
 
 
+# №2
+#вариант 12
+#Дан файл, содержащий зашифрованный русский текст. Каждая буква заменяется на следующую за ней (буква я заменяется на а). Получить в новом файле расшифровку данного текста.
+
+f2 = open('code_file.txt', 'r', encoding='utf-8')
+g2 = open('decode_file.txt', 'w')
+
+alph='абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+text=f2.read()
+text=text.lower()
+res=''
+for i in text:
+    if(i.isalpha()):
+        index=alph.find(i)
+        #res += alph[(index + 1)%33]
+        if(index==0):
+            res+=alph[32]
+        else:
+            res+=alph[(index-1)]
+    else:
+        res+=i
+print(res)
+g2.write(res)
+f2.close()
+g2.close()
+
